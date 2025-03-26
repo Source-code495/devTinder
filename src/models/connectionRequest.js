70ae22187e6da3bf4eae93225d1a839857a8b1a9
kush's <patelkushgara@gamil.com>
2025-03-26 20:25:49 +0530

    initial commit

---
+++ src/models/connectionRequest.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const connectionRequestSchema = new mongoose.Schema(
+  {
+    fromUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref :"User",
+      required: true,
+    },
+    toUserId: {
+      // ab toUserId point kr rha h User model ko 
+      type: mongoose.Schema.Types.ObjectId,
+      ref : "User",
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: {
+        values: ["ignored", "interested", "accepted", "rejected"],
+        message: `{$VALUE} is incorrect status type`,
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+connectionRequestSchema.index({ fromUserId: 1, toUserId: 1 });
+
+connectionRequestSchema.pre("save", function (next) { // we can create this on request ,pr ye bas likhne ka tarika h
+  const connectionRequest = this;
+  // check if the fromUserId is same as toUserId
+  if (connectionRequest.fromUserId.equals(connectionRequest.toUserId)) {
+    throw new Error("Cannot sent connection request to ypurself !!");
+  }
+  next();
+});
+
+const connectionRequestModel = new mongoose.model(
+  "connectionRequest",
+  connectionRequestSchema
+);
+module.exports = connectionRequestModel;
---
+++ src/routes/profile.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const profileRouter = express.Router();
+
+const { userAuth } = require("../middlewares/auth");
+const { validateEditProfileData } = require("../utils/validation");
+
+profileRouter.get("/profile/view", userAuth, async (req, res) => {
+  try {
+    const user = req.user;
+
+    res.send(user);
+  } catch (err) {
+    res.status(400).send("ERROR : " + err.message);
+  }
+});
+
+profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
+  try {
+    // console.log(1);
+    // res.send("Edit Profile Route");
+    if (!validateEditProfileData(req)) {
+      throw new Error("Invalid Edit Request");
+    }
+
+    const loggedInUser = req.user;
+
+    Object.keys(req.body).forEach((key) => (loggedInUser[key] = req.body[key])); // ye db pr nhi update kre ga 
+
+    await loggedInUser.save(); // save krne k badd db pr update hoga 
+
+    res.json({
+      message: `${loggedInUser.firstName}, your profile updated successfuly`,
+      data: loggedInUser,
+    });
+  } catch (err) {
+    res.status(400).send("ERROR : " + err.message);
+  }
+});
+
+module.exports = profileRouter;
